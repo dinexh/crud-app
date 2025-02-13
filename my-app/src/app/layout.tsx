76b1525b639399be6be0c360574833713bd6ffcf
Dinesh korukonda <dineshkorukonda05@gmail.com>
2025-02-13 16:35:24 +0530

    added next app and basic setup

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-poppins',
+});
+
+export const metadata: Metadata = {
+  title: "CRUD Application",
+  description: "CRUD Application - Web Assignment",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="home-component">
+      <h1>CRUD Application</h1>
+   </div>
+  );
+}
